3c6d98fc48879ff4ea4186b75e355745d1e70cbb
celebalane <celebalane@gmail.com>
2017-10-12 15:25:45 +0200

    routeur

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -4,18 +4,19 @@ namespace OC\PlatformBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class AdvertController extends Controller
 {
     public function indexAction()
     {
-    	$content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig',array('nom'=>'Mango'));
-        return new Response($content);
+    	/*$content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig',array('nom'=>'Mango'));*/
+    	$url = $this->generateUrl('oc_platform_view', array('id'=>'5'),UrlGeneratorInterface::ABSOLUTE_URL);
+        return new Response("L'url est : ".$url);
     }
 
-    public function byeAction()
+    public function viewAction($id)
     {
-    	$content = $this->get('templating')->render('OCPlatformBundle:Advert:bye.html.twig',array('nom'=>'Petit chat'));
-    	return new Response($content);
+    	return new Response("Affichage de l'annonce : ".$id);
     }
 }
